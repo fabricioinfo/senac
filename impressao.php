dc6249cd0ef953711a106f0997e265ebb3d2d39d
ghoulbr <62670492+ghoulbr@users.noreply.github.com>
2020-03-28 20:21:42 -0300

    Update impressao.php

--- impressao.php
+++ impressao.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 	<head>
-		<title>Folha de Presença</title>
+		<title>Folha de Presença Teste</title>
 		<meta charset="utf-8"/>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 		
